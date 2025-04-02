d93bbd330f936f71d815f369a075872f07402a2e
Damn <51714144+damndemento@users.noreply.github.com>
2025-04-02 17:09:51 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ cd /opt/network-manager
 source venv/bin/activate
 
 # Set environment variables
-export FLASK_SECRET_KEY="c5521ab2e51b07b079aad5258ffb8ff719f40e6925f19e8d184c059b1f25bd68"
+export FLASK_SECRET_KEY="<my-flask-secret-code>"
 export FLASK_PORT=5000
 export FLASK_BIND_ADDRESS="0.0.0.0"
 
